177f041a22c7f8affab6da0d5abea7ed3d2dc4f7
sigma1118 <sigma1118@gmail.com>
2025-06-02 16:08:01 -0400

    Update index.ts
    Removed jokes tools

--- src/index.ts
+++ src/index.ts
@@ -4,30 +4,10 @@ import { SSEServerTransport } from "@modelcontextprotocol/sdk/server/sse.js";
 import { z } from "zod";
 
 const server = new McpServer({
-  name: "jokesMCP",
-  description: "A server that provides jokes",
+  name: "GeographyMCP",
+  description: "A server that geography information",
   version: "1.0.0",
   tools: [
-    {
-      name: "get-chuck-joke",
-      description: "Get a random Chuck Norris joke",
-      parameters: {},
-    },
-    {
-      name: "get-chuck-categories",
-      description: "Get all available categories for Chuck Norris jokes",
-      parameters: {},
-    },
-    {
-      name: "get-dad-joke",
-      description: "Get a random dad joke",
-      parameters: {},
-    },
-    {
-      name: "get-yo-mama-joke",
-      description: "Get a random Yo Mama joke",
-      parameters: {},
-    },
      {
       name: "get-country-data",
       description: "Get a country data by name",
@@ -45,6 +25,7 @@ const server = new McpServer({
     },
   ],
 });
+// City Details tool
 const getCityDetails = server.tool(
   "get-city-details",
   {
@@ -140,64 +121,6 @@ const getZipInfo = server.tool(
     }
   }
 );
-
-// Get Chuck Norris joke tool
-const getChuckJoke = server.tool(
-  "get-chuck-joke",
-  "Get a random Chuck Norris joke",
-  async () => {
-    const response = await fetch("https://api.chucknorris.io/jokes/random");
-    const data = await response.json();
-    return {
-      content: [
-        {
-          type: "text",
-          text: data.value,
-        },
-      ],
-    };
-  }
-);
-
-// Get Chuck Norris joke categories tool
-const getChuckCategories = server.tool(
-  "get-chuck-categories",
-  "Get all available categories for Chuck Norris jokes",
-  async () => {
-    const response = await fetch("https://api.chucknorris.io/jokes/categories");
-    const data = await response.json();
-    return {
-      content: [
-        {
-          type: "text",
-          text: data.join(", "),
-        },
-      ],
-    };
-  }
-);
-
-// Get Dad joke tool
-const getDadJoke = server.tool(
-  "get-dad-joke",
-  "Get a random dad joke",
-  async () => {
-    const response = await fetch("https://icanhazdadjoke.com/", {
-      headers: {
-        Accept: "application/json",
-      },
-    });
-    const data = await response.json();
-    return {
-      content: [
-        {
-          type: "text",
-          text: data.joke,
-        },
-      ],
-    };
-  }
-);
 // 2025-05-29 - Add get-country-data tool
 const getCountryData = server.tool(
   "get-country-data",
@@ -237,25 +160,6 @@ const getCountryData = server.tool(
         {
           type: "text",
           text: country.flags?.png || "No flag found",
-        },
-      ],
-    };
-  }
-);
-// Get Yo Mama joke tool
-const getYoMamaJoke = server.tool(
-  "get-yo-mama-joke",
-  "Get a random Yo Mama joke",
-  async () => {
-    const response = await fetch(
-      "https://www.yomama-jokes.com/api/v1/jokes/random"
-    );
-    const data = await response.json();
-    return {
-      content: [
-        {
-          type: "text",
-          text: data.joke,
         },
       ],
     };
